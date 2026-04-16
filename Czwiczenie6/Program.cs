80aa1d35aef99ee6eeba85cb1db874c6b308464e
Denys Boiko <denisbojko2005@gmail.com>
2026-04-16 17:48:08 +0200

    add base Program.cs with swagger

---
+++ Czwiczenie6/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = false;
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
