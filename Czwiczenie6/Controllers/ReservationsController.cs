e04d31b9850a4ba3a1a469888c83594d35d831ac
Denys Boiko <denisbojko2005@gmail.com>
2026-04-19 13:33:58 +0200

    add method CreateReservation

--- Czwiczenie6/Controllers/ReservationsController.cs
+++ Czwiczenie6/Controllers/ReservationsController.cs
@@ -44,4 +44,34 @@ public class ReservationsController : ControllerBase
 
         return Ok(reservation);
     }
+
+    [HttpPost]
+    public ActionResult<Reservation> CreateReservation([FromBody] Reservation reservation)
+    {
+        var room = AppData.Rooms.FirstOrDefault(r => r.Id == reservation.RoomId);
+        if (room == null)
+        {
+            return BadRequest("Invalid room Id");
+        }
+
+        if (!room.isActive)
+        {
+            return BadRequest("Can not create reservation for inactive room");
+        }
+
+        var isReservation = AppData.Reservations.Any(r =>
+            r.RoomId == reservation.RoomId &&
+            r.Date == reservation.Date &&
+            reservation.StartTime < r.EndTime &&
+            reservation.EndTime > r.StartTime);
+        if (isReservation)
+        {
+            return Conflict("It is another reservation for this room for this time");
+        }
+
+        var newId = AppData.Reservations.Max(r => r.Id) + 1;
+        reservation.Id = newId;
+        AppData.Reservations.Add(reservation);
+        return CreatedAtAction(nameof(GetReservationById), new { id = reservation.Id },reservation);
+    }
 }
